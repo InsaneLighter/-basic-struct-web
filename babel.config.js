34137d1482396f3897b9aaae2fbb654ca4d84722
huanghong <huanghong2@apexsoft.com.cn>
2022-03-30 20:55:05 +0800

    dev: basic config files commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+const plugins = []
+
+// 生产环境移除console
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console')
+}
+
 module.exports = {
+  plugins: plugins,
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    '@vue/app'
   ]
 }
---
+++ vue.config.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const path = require('path')
+const port = 17777
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  publicPath: process.env.PUBLIC_PATH,
+  //css配置
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+        },
+        javascriptEnabled: true
+      }
+    }
+  },
+  //开发服务器配置
+  devServer: {
+    host: "localhost",
+    //端口号
+    port: port,
+    //自动启动浏览器
+    open: true,
+    //代理
+    proxy: {}
+  },
+  configureWebpack: {
+    name: name,
+    resolve: {
+      alias: {
+        // 包别名设置
+        '@': resolve('src'),
+      }
+    }
+  },
+  lintOnSave: false,
+  transpileDependencies: [],
+  productionSourceMap: false
+}
