572af808c1d871c4da9ada17bb2f7e4da9ff89fd
huanghong <huanghong2@apexsoft.com.cn>
2022-03-31 21:13:03 +0800

    dev: add layout component

--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -1,21 +1,33 @@
 <template>
-  <div>
-    <h1>Layout</h1>
-    <router-view/>
+  <a-layout id="layout">
+    <global-side-menu></global-side-menu>
+    <global-header>
+      <a-layout-content
+          :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '800px' }"
+      >
+        <router-view/>
+      </a-layout-content>
+    </global-header>
     <global-footer></global-footer>
-  </div>
+  </a-layout>
 </template>
 
 <script>
 import GlobalFooter from './global/GlobalFooter'
+import GlobalHeader from './global/GlobalHeader'
+import GlobalSideMenu from './global/GlobalSideMenu'
 export default {
   name: "Layout",
   components: {
-    GlobalFooter
+    GlobalFooter,
+    GlobalHeader,
+    GlobalSideMenu
   }
 }
 </script>
 
 <style scoped>
-
+#layout{
+  height: 100%;
+}
 </style>
---
+++ src/components/layouts/global/GlobalHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <a-layout>
+    <a-layout-header style="background: #fff; padding: 0">
+      <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="() => (collapsed = !collapsed)"
+      />
+      <slot></slot>
+    </a-layout-header>
+  </a-layout>
+</template>
+<script>
+export default {
+  name: 'GlobalHeader',
+  data() {
+    return {
+      collapsed: false,
+    };
+  }
+};
+</script>
+<style scoped>
+.trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+.trigger:hover {
+  color: #1890ff;
+}
+
+.logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
